c025b372b361808589fad50d3d196d8bc2928006
Redouan-El-Khanoussi <63659937+Redouan-El-Khanoussi@users.noreply.github.com>
2021-09-28 16:04:50 +0000

    First commit

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -6,15 +6,13 @@
 	<script src="views/includes/vendor/bootstrap/js/popper.js"></script>
 	<script src="views/includes/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="views/includes/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
 	<script src="views/includes/vendor/tilt/tilt.jquery.min.js"></script>
+<!--===============================================================================================-->
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-<!--===============================================================================================-->
-	<script src="views/includes/js/main.js"></script>
+
 </body>
 </html>
